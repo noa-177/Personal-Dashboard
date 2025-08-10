a2390c0f67e62194f51a3ebaeaedb6b332f1c988
noa-177 <118047174+noa-177@users.noreply.github.com>
2025-08-10 07:34:48 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// DOM Elements
+const form = document.getElementById('budget-form');
+const categoryInput = document.getElementById('category');
+const amountInput = document.getElementById('amount');
+const salaryInput = document.getElementById('salary-input');
+const currencySelect = document.getElementById('currency-select');
+
+const cardSalary = document.getElementById('card-salary');
+const cardExpenses = document.getElementById('card-expenses');
+const cardRemaining = document.getElementById('card-remaining');
